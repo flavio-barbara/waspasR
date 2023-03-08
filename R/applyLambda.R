36e8fe6adaf51d4084fea725039e03be8eeab597
flavio-barbara <flavio.barbara@gmail.com>
2023-03-08 12:45:02 +0000

    more convention

--- R/applyLambda.R
+++ R/applyLambda.R
@@ -35,7 +35,7 @@ applyLambda <- function(matrix_wsm, matrix_wpm, lambda) {
       return("Error: WSM & WPM matrices entered must have same number of rows")
     }
     # WASPAS Ranking
-    waspas_matrix <- cbind(matrix_wsm, matrix_wpm[, "points"], WASPAS = 0.0)
+    waspas_matrix <- cbind(matrix_wsm, matrix_wpm[, "WPM_Rank"], WASPAS = 0.0)
     colnames(waspas_matrix) <- c("Alternative"
                                  , "WSM_Rank", "WPM_Rank", "WASPAS_Rank")
     waspas_matrix[, "WASPAS_Rank"] <-
